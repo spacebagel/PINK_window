5ecd2e80ce416f43732c698f9b8ad98f77ae80c8
spacebagel <murkly@proton.me>
2024-07-02 11:41:01 +0300

    Добавьте файлы проекта.

---
+++ PINK_window/Windows/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace PINK_window.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Window;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window = this;
+        }
+
+        private void Drag(object sender, RoutedEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed) MainWindow.Window.DragMove();
+        }
+
+        private void CloseBtnClick(object sender, MouseButtonEventArgs e) => Application.Current.Shutdown();
+        private void MinimizeBtnClick(object sender, MouseButtonEventArgs e) => WindowState = WindowState.Minimized;
+        private void MaximizeBtnClick(object sender, MouseButtonEventArgs e) =>
+            WindowState = WindowState != WindowState.Maximized ? WindowState.Maximized : WindowState.Normal;
+    }
+}
